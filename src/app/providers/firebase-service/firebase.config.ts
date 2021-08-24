8057e572372dfb7e0bdb4a449e1a72ca4516409d
amol15 <amol.chaware@biz4solutions.com>
2021-08-24 15:28:14 +0530

    firebase new setup

--- src/app/providers/firebase-service/firebase.config.ts
+++ src/app/providers/firebase-service/firebase.config.ts
@@ -1,29 +1,26 @@
 export const firebaseConfig = {
   Local: {
-    apiKey: 'AIzaSyAX64ZhXH0ap-KTsLi_vAogT1MnGv8LwqQ',
-    authDomain: 'ecgclinicalcases-69309.firebaseapp.com',
-    projectId: 'ecgclinicalcases-69309',
-    storageBucket: 'ecgclinicalcases-69309.appspot.com',
-    messagingSenderId: '877896401787',
-    appId: '1:877896401787:web:26edde39f528091f98f3de',
-    measurementId: 'G-YMX7RH2Y19',
+      apiKey: "AIzaSyDwKtHFC5neZy02yv-1EgjbUUfu77TIExE",
+      authDomain: "ecg-library.firebaseapp.com",
+      projectId: "ecg-library",
+      storageBucket: "ecg-library.appspot.com",
+      messagingSenderId: "913576419098",
+      appId: "1:913576419098:web:50136df03deeb2d443d6a3"
   },
   Test: {
-    apiKey: 'AIzaSyAX64ZhXH0ap-KTsLi_vAogT1MnGv8LwqQ',
-    authDomain: 'ecgclinicalcases-69309.firebaseapp.com',
-    projectId: 'ecgclinicalcases-69309',
-    storageBucket: 'ecgclinicalcases-69309.appspot.com',
-    messagingSenderId: '877896401787',
-    appId: '1:877896401787:web:26edde39f528091f98f3de',
-    measurementId: 'G-YMX7RH2Y19',
+    apiKey: "AIzaSyDwKtHFC5neZy02yv-1EgjbUUfu77TIExE",
+    authDomain: "ecg-library.firebaseapp.com",
+    projectId: "ecg-library",
+    storageBucket: "ecg-library.appspot.com",
+    messagingSenderId: "913576419098",
+    appId: "1:913576419098:web:50136df03deeb2d443d6a3"
   },
   Production: {
-    apiKey: 'AIzaSyDfDNW3cKUlKN06WOFkfGSIcx-Txdf8LkY',
-    authDomain: 'ecg-corner.firebaseapp.com',
-    projectId: 'ecg-corner',
-    storageBucket: 'ecg-corner.appspot.com',
-    messagingSenderId: '247707053126',
-    appId: '1:247707053126:web:c51721de0a174ca76e2e47',
-    measurementId: 'G-JEJCKMK5LN',
+    apiKey: "AIzaSyDwKtHFC5neZy02yv-1EgjbUUfu77TIExE",
+    authDomain: "ecg-library.firebaseapp.com",
+    projectId: "ecg-library",
+    storageBucket: "ecg-library.appspot.com",
+    messagingSenderId: "913576419098",
+    appId: "1:913576419098:web:50136df03deeb2d443d6a3"
   },
 };
